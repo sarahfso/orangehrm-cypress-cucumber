8fdacfd58e6bfb21af94d9498d4b1c8d780fd216
sarahfso <sarahfso@users.noreply.github.com>
2023-11-06 17:38:45 +0000

    deploy: 5e27cd208bf97187d256075034b79083d01b9b98

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('shouldRedirect', (endpoint) => {
-    cy.url().should('include', endpoint);
-})
-
-Cypress.Commands.add('selectInputByLabel', (label) => {
-    cy.contains('label', label).parent().next().find('input').should('be.visible');
-})
